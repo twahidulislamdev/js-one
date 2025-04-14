d2c96a74c22ef17fc8565ae0d9c3c1e8b7a068f4
Twahidul Islam <twahidulislam2005@gmail.com>
2025-04-15 00:50:01 +0600

    first commit

---
+++ js/variable.js
@@ -0,0 +1,47 @@
+// var variable
+
+// var name = "Twahidul Islam"
+// var name = "Emon"
+// console.log(name);
+
+// let Variable
+
+// let product = "Mango"
+// product = "Banana"
+// console.log(product)
+
+// const variable
+
+// const pai = 3.1416
+// console.log(pai);
+
+// let cap = 100
+// let shirt = 1500
+// console.log(cap+shirt);
+
+// let book = 315
+// let pen = 20
+// let sum = book + pen
+// console.log(sum);
+
+// Primary Scope 
+{
+  let x = 500;
+  console.log(x);
+}
+// Public Scope 
+{
+    var y = 300
+}
+console.log(y);
+
+let z = "Twahhidul"
+console.log(typeof(z));
+
+let a = 55
+console.log(typeof(a));
+
+let b = true
+console.log(typeof(b));
+
+
